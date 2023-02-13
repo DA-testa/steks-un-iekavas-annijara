721ae52929ef5636a2357750e6a5f1d00a4fe68d
annijara <99959888+annijara@users.noreply.github.com>
2023-02-13 10:40:07 +0200

    kaut kas

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i + 1))
             pass
 
         if next in ")]}":
