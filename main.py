e761ca50d21098565542363605cacdaf4c1289e7
annijara <99959888+annijara@users.noreply.github.com>
2023-02-19 13:35:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,13 @@ def are_matching(left, right):
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
-        if next in "([{":
-            opening_brackets_stack.append(Bracket(next, i + 1)) 
+        if next in "([{":opening_brackets_stack.append(Bracket(next, i+1)) 
         if next in ")]}":
-            if not opening_brackets_stack or are_matching(opening_brackets_stack[-1].char, next):
-                return 1+i
-        opening_brackets_stack.pop()
-        if opening_brackets_stack:
+            if not opening_brackets_stack or are_matching(opening_brackets_stack[-1].char, next):return 1+i
+            opening_brackets_stack.pop()
+            if opening_brackets_stack:
                 return opening_brackets_stack[0].position
-        return"Success"
+    return"Success"
 
 
 def main():
