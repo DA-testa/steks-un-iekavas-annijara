e1660770ed60e7966180dfc0f8018be6f0482660
annijara <99959888+annijara@users.noreply.github.com>
2023-02-19 13:20:59 +0200

    NOP

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ def find_mismatch(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i + 1)) 
         if next in ")]}":
-                if not opening_brackets_stack or are_matching(opening_brackets_stack[-1].char, next):
-                    return 1+i
-                opening_brackets_stack.pop()
-                if opening_brackets_stack:
-                    return opening_brackets_stack[0].position
-                return"Success"
+            if not opening_brackets_stack or are_matching(opening_brackets_stack[-1].char, next):
+                return 1+i
+        opening_brackets_stack.pop()
+        if opening_brackets_stack:
+                return opening_brackets_stack[0].position
+        return"Success"
 
 
 def main():
-    #s = input()
-    #if s == "I" or s == "F":
+    s = input()
+    if s == "I" or s == "F":
         text = input()
         mismatch = find_mismatch(text)
         print(mismatch)
