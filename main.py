5fcc2f56135cb06c2f2a85791d725b9cad29f809
annijara <99959888+annijara@users.noreply.github.com>
2023-02-19 15:39:57 +0200

    done??

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # python3
+
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
+
 
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
@@ -11,37 +13,19 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next, i + 1)) 
+          opening_brackets_stack.append(Bracket(next,i+1))  
         if next in ")]}":
-            if not opening_brackets_stack or are_matching(opening_brackets_stack[-1].char, next):
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
             opening_brackets_stack.pop()
-            if opening_brackets_stack:
-                return opening_brackets_stack[0].position
-        return"Success"
-
-"""    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            opening_brackets_stack.append(Bracket(next, i+1)) 
-        if next in ")]}":
-            if not opening_brackets_stack or are_matching(opening_brackets_stack[-1].char, next):
-                return i+1
-            opening_brackets_stack.pop()
-        if opening_brackets_stack:
-            return opening_brackets_stack[0].position
-    return"Success" """
-
+    if opening_brackets_stack:
+        return opening_brackets_stack[-1].position
+    return "Success"
+              
 
 def main():
-    #I un F izvade
-    #s = input()
-    #if s=="I" or "F":
-    input("I")
     text = input()
     mismatch = find_mismatch(text)
     print(mismatch)
 
-if __name__ == "__main__":
-    #input("I")
-    main()
+main()
