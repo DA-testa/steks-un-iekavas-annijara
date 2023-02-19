4c2d307cdeda3988231e418cade0e5b408ec8bf9
annijara <99959888+annijara@users.noreply.github.com>
2023-02-19 12:34:36 +0200

    kk

--- main.py
+++ main.py
@@ -10,34 +10,27 @@ def are_matching(left, right):
 
 
 def find_mismatch(text):
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            opening_brackets_stack.append(Bracket(next, i + 1)) 
-            #pass  ja nevēlās, ka kaut kas izpildās...DZĒST!!!
+        opening_brackets_stack = []
+        for i, next in enumerate(text):
+            if next in "([{":
+                opening_brackets_stack.append(Bracket(next, i + 1)) 
 
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            if not opening_brackets_stack or opening_brackets_stack[-1] != next:
-                return 1+i
-            opening_brackets_stack.pop()
-        if opening_brackets_stack:
-            return opening_brackets_stack[0].position
-        
-               # pass
-           # opening_brackets_stack.pop()
-         # !opening_brackets_stack.top().Matchc(next))
+            if next in ")]}":
+                if not opening_brackets_stack or are_matching(opening_brackets_stack[-1].char, next):
+                    return 1+i
+                opening_brackets_stack.pop()
+                if opening_brackets_stack:
+                        return opening_brackets_stack[0].position
+            return"Success"
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    if not mismatch :
-        print("Success")
-    else:
+    print("Ievadiet I vai F:")
+    s = input()
+    if s == "I" :
+        text = input()
+        mismatch = find_mismatch(text)
         print(mismatch)
-    # Printing answer, write your code here
 
 
 if __name__ == "__main__":
