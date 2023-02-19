1d098edc5ebc129c2b07a8152b53adb73b369903
annijara <99959888+annijara@users.noreply.github.com>
2023-02-19 13:53:11 +0200

    kk

--- main.py
+++ main.py
@@ -14,18 +14,20 @@ def find_mismatch(text):
             opening_brackets_stack.append(Bracket(next, i+1)) 
         if next in ")]}":
             if not opening_brackets_stack or are_matching(opening_brackets_stack[-1].char, next):
-                return 1+i
-    opening_brackets_stack.pop()
-    if opening_brackets_stack:
-                return opening_brackets_stack[0].position
+                return i+1
+            opening_brackets_stack.pop()
+        if opening_brackets_stack:
+            return opening_brackets_stack[0].position
     return"Success"
 
 
 def main():
+    #I un F izvade
+    #s = input()
     text = input()
     mismatch = find_mismatch(text)
     print(mismatch)
 
 if __name__ == "__main__":
-    input("I")
+    #input("I")
     main()
